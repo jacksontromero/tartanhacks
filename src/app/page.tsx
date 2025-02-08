f513bd194d151c143f072460d08e3a10b5e3bfff
Jackson Romero <jacksontromero@gmail.com>
2025-02-08 11:46:16 -0500

    add host events carosuel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
+import { eq } from "drizzle-orm";
 import SignInButton from "~/components/auth/SignInButton";
 import HostEventButton from "~/components/events/HostEventButton";
+import YourEvents from "~/components/events/YourEvents";
 import TestInput from "~/components/gemini/TestInput";
 import { auth } from "~/server/auth";
+import { db } from "~/server/db";
+import { events } from "~/server/db/schema";
+
+async function getEvents(userId: string) {
+  const myEvents = await db.query.events.findMany({
+    where: eq(events.hostId, userId),
+    with: {
+      responses: true,
+    },
+  });
+  return myEvents;
+}
+
+export type EventWithResponses = Awaited<ReturnType<typeof getEvents>>[number];
 
 export default async function HomePage() {
 
   const session = await auth();
+
+  let events: EventWithResponses[] = [];
+
+  if (session && session.user) {
+    events = await getEvents(session.user.id);
+  }
 
   return (
     <main className="flex h-[calc(100vh-66px)] flex-col items-center justify-center bg-background">
@@ -17,6 +39,7 @@ export default async function HomePage() {
           <SignInButton />
           {session && <HostEventButton />}
         </div>
+        <YourEvents events={events} />
         <TestInput />
       </div>
     </main>
